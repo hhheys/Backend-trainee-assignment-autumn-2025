30282c451b0e361ab23266917a9b2d470748cf5f
hhheys <dmitriy.tsyganckov@gmail.com>
2025-11-16 11:26:53 +0300

    Code refactoring

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -57,13 +57,14 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatal("Server Shutdown:", err)
 	}
-	// catching ctx.Done(). timeout of 5 seconds.
-	select {
-	case <-ctx.Done():
+
+	if errors.Is(ctx.Err(), context.DeadlineExceeded) {
 		log.Println("timeout of 5 seconds.")
 	}
+
 	log.Println("Server exiting")
 }
